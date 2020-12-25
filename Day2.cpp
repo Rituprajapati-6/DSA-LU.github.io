a9f29c646e832effead804b48a078ab1b89bd082
Ritu Prajapati <56599339+Rituprajapati-6@users.noreply.github.com>
2020-12-25 21:09:17 +0530

    Create Day2.cpp

---
+++ Day2.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+int arr[30];
+int n, i;
+
+//TO DELETE ELEMENT AT THE INDEX OR BEGINNING OF AN ARRAY
+void delelteBeginning(){
+	cout<<"\nAfter deletion, ";
+	for (i=0;i<n;i++){
+	arr[i]=arr[i+1];
+}
+n--;
+}
+
+//TO DELETE ELEMENT AT THE GIVEN POSITION OF AN ARRAY
+void deleteIndex(int ind){
+	cout<<"\nAfter deleting element at "<<ind<<"position, ";
+	for (i=ind;i<n;i++){
+	arr[i]=arr[i+1];
+}
+n--;
+}
+
+//TO ROTATE THE ARRAY BY NO. OF TIMES INPUT BY USER
+void rotate() {
+	int k,j;
+	cout<<"enter no. of rotations: ";
+	cin>>k;
+	cout<<"\nAfter rotating "<<k<<" times, ";
+	for (j=0;j<k;j++){
+	int temp = arr[0];
+	for (i=0;i<n-1;i++){
+		arr[i] = arr[i+1];
+	}
+	arr[i]=temp;
+	}
+}
+
+//TO DISPLAY ELEMENTS OF THE ARRAY
+display(){
+	cout<<"Array is: ";
+for (i =0; i<n; i++)
+{
+	cout<<arr[i]<<"  ";
+}cout<<"\n";	
+}
+
+//DRIVER CODE
+int main() {
+	cout<<"enter size of arr: ";
+	cin>>n;
+	cout<<"Elements: ";
+	for(int i =0;i<n;i++){
+		cin>>arr[i];
+	}
+	
+display();
+rotate();
+display();
+delelteBeginning();
+display();
+deleteIndex(2);
+display();
+}
